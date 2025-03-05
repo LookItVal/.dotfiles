e9c5d01360c31e114cea0b4101eae0aaf5a0203a
Quinn Valencia Cecil <86853474+LookItVal@users.noreply.github.com>
2025-03-04 19:55:51 -0600

    cattpuccin spotify theme

--- init/arch.sh
+++ init/arch.sh
@@ -31,9 +31,22 @@ sudo cp .dotfiles/arch/wallpaper.timer /etc/systemd/system/wallpaper.timer
 sudo cp .dotfiles/arch/wallpaper.sh /etc/systemd/system/wallpaper.sh
 sudo mkdir /etc/systemd/system/wallpapers
 sudo cp .dotfiles/arch/wallpapers/* /etc/systemd/system/wallpapers
+sudo mkdir /etc/systemd/system/getty@tty1.service.d
+sudo cp .dotfiles/arch/autologin.conf /etc/systemd/system/getty@tty1.service.d/autologin.conf
 systemctl enable wallpaper.timer
+systemctl enable wallpaper.service
+systemctl start wallpaper.service
 systemctl start wallpaper.timer
 
 sudo pacman -Syu
 sudo pacman -S --noconfirm exa bat ripgrep fd fzf neovim alacritty i3-wm xorg xorg-xinit xorg-server i3lock i3status rofi pulseaudio pavucontrol firefox feh spotify-launcher i3blocks nodejs python ipython python-pip nerd-fonts vlc
-yay visual-studio-code-bin i3lock-color micromamba nvm
+yay visual-studio-code-bin
+yay i3lock-color
+yay micromamba
+yay nvm
+yay spicetify-cli
+
+spicetify config current_theme catppuccin
+spicetify config color_scheme macchiato
+spicetify config inject_css 1 inject_theme_js 1 replace_colors 1 overwrite_assets 1
+spicetify apply
