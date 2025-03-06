b3b032757c130da58d11aeede665bdf25630fa9a
Quinn Valencia Cecil <86853474+LookItVal@users.noreply.github.com>
2025-03-05 18:06:50 -0600

    autologin

--- init/arch.sh
+++ init/arch.sh
@@ -34,20 +34,23 @@ sudo cp .dotfiles/arch/wallpapers/* /etc/systemd/system/wallpapers
 sudo mkdir /etc/systemd/system/getty@tty1.service.d
 sudo cp .dotfiles/arch/autologin.conf /etc/systemd/system/getty@tty1.service.d/autologin.conf
 systemctl enable wallpaper.timer
-systemctl enable wallpaper.service
-systemctl start wallpaper.service
 systemctl start wallpaper.timer
 
 sudo pacman -Syu
 sudo pacman -S --noconfirm exa bat ripgrep fd fzf neovim alacritty i3-wm xorg xorg-xinit xorg-server i3lock i3status rofi pulseaudio pavucontrol firefox feh spotify-launcher i3blocks nodejs python ipython python-pip nerd-fonts vlc thunar thunar-volman ark thunar-archive-plugin gvfs
-yay visual-studio-code-bin
-yay i3lock-color
-yay micromamba
-yay nvm
-yay spicetify-cli
-yay networkmanager-dmenu-git
+for pkg in visual-studio-code-bin i3lock-color micromamba nvm spicetify-cli networkmanager-dmenu-git; do
+  if ! yay -Q $pkg &>/dev/null; then
+    yay -S --noconfirm $pkg
+  else
+    echo "$pkg is already installed"
+  fi
+done
 
-spicetify config current_theme catppuccin
-spicetify config color_scheme macchiato
-spicetify config inject_css 1 inject_theme_js 1 replace_colors 1 overwrite_assets 1
-spicetify apply
+echo "Would you like to apply the catppuccino theme to Spotify? (y/n)"
+read -r response
+if [[ $response == "y" ]]; then
+  spicetify config current_theme catppuccino
+  spicetify config color_scheme macchiato
+  spicetify config inject_css 1 inject_theme_js 1 replace_colors 1 overwrite_assets 1
+  spicetify apply
+fi
