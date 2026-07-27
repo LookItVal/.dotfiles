242092e4db2dffe39dc1915340fe5d2708d9c7b3
Quinn Valencia Cecil <86853474+LookItVal@users.noreply.github.com>
2026-07-27 17:12:11 -0500

    gtk theming and picom to work with it

--- init/arch.sh
+++ init/arch.sh
@@ -38,7 +38,7 @@ systemctl start wallpaper.timer
 
 sudo pacman -Syu
 sudo pacman -S --noconfirm ntp exa bat ripgrep fd fzf neovim alacritty i3-wm picom xorg xorg-xinit xorg-server i3status rofi pipewire pipewire-pulse firefox feh spotify-launcher i3blocks nodejs python ipython python-pip nerd-fonts vlc thunar thunar-volman ark thunar-archive-plugin gvfs htop nvtop flameshot dunst xclip xsel
-for pkg in visual-studio-code-bin i3lock-color micromamba nvm spicetify-cli networkmanager-dmenu-git networkmanager-dispatcher-ntpd pwvucontrol; do
+for pkg in visual-studio-code-bin i3lock-color nvm spicetify-cli networkmanager-dmenu-git networkmanager-dispatcher-ntpd pwvucontrol catppuccin-gtk-theme-macchiato; do
   if ! yay -Q $pkg &>/dev/null; then
     yay -S --noconfirm --answerclean All --overwrite="*" $pkg
   else
